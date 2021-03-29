7a43e0fcdf7971079152d6b88c7f8c8cd177aa76
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2021-03-29 15:42:14 +0100

    Added UI translations

---
+++ js/plugins/CustomizeOptions.js
@@ -0,0 +1,9 @@
+Window_Options.prototype.addGeneralOptions = function() {
+
+};
+
+Window_Options.prototype.addVolumeOptions = function() {
+    this.addCommand("Background Volume", 'bgmVolume');
+    this.addCommand("Effects Volume", 'seVolume');
+
+};
