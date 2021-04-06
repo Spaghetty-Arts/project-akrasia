268f5be1241df4451d6f874e47f78349c7a59d46
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2021-04-06 20:16:12 +0100

    Collisions city WIP

---
+++ js/plugins/CarMovement.js
@@ -0,0 +1,22 @@
+//=============================================================================
+// CarMovement.js
+//=============================================================================
+/*:
+ * @plugindesc Code for altering the palyer color
+ *
+ * @author fabian
+ *
+ * @help
+ *
+ * Code for  altering the palyer color
+ *
+ *
+ */
+
+(function () {
+
+   detectPlayerColission = function () {
+
+   }
+
+})();
