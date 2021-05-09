e7fbfe9f78b0c06f67de5fad0a6424fc280c81a3
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2021-05-09 09:47:45 +0100

    Remove unused plugins

--- js/plugins/!SPF_GameOver.js
+++
@@ -1,6 +0,0 @@
-Scene_Gameover.prototype.update = function() {
-    if (this.isActive() && !this.isBusy() && this.isTriggered()) {
-        SceneManager.push(Scene_GameEnd);
-    }
-    Scene_Base.prototype.update.call(this);
-};
